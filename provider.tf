643280479fbfdf10f65d52495cd2f92cf1c17a22
alejandro <achdevops@gmail.com>
2021-04-06 23:52:24 -0300

    set versions

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  #required_version = ">= 0.13"
+  required_version = ">= 0.13"
 }
 
 provider "helm" {
-  #version = "~> 1.3.2"
+  version = "~> 1.3.2"
   kubernetes {
     #define locally $HOME/.kube/config
     #https://registry.terraform.io/providers/hashicorp/helm/latest/docs
