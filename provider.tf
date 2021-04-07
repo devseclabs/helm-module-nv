74b9f592434767f5268f89cb14f94481f5eb4e1d
alejandro <achdevops@gmail.com>
2021-04-07 00:39:20 -0300

    update provider

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@ terraform {
   #required_version = ">= 0.13"
 }
 
-provider "kubernetes" {
-  config_path    = var.kube_path
-  config_context = var.kube_context
-}
+# provider "kubernetes" {
+#   config_path    = var.kube_path
+#   config_context = var.kube_context
+# }
 
 provider "helm" {
   #version = "~> 1.3.2"
