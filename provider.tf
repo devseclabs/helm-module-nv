355f19dbe9bffff4554097a81b3f16435f615991
alejandro <achdevops@gmail.com>
2021-04-07 00:09:42 -0300

    update context

--- provider.tf
+++ provider.tf
@@ -2,13 +2,12 @@ terraform {
   required_version = ">= 0.13"
 }
 
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  # UPDATE YOUR CONTEXT
+  config_context = var.context
+}
+
 provider "helm" {
   version = "~> 1.3.2"
-  kubernetes {
-    #define locally $HOME/.kube/config
-    #https://registry.terraform.io/providers/hashicorp/helm/latest/docs
-    config_path    = "~/.kube/config"
-    # UPDATE YOUR CONTEXT
-    config_context = var.context
-   }
 }
